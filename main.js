373f25f1f182255a3621f40382fad35b8658e728
Kartikeyan-rai <73011067+Kartikeyan-rai@users.noreply.github.com>
2021-06-16 19:31:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(250,300);
+    canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('My_filter_image.png')
+}
